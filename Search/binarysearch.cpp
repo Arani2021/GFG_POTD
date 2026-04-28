31a07d11fb8529fc9271188042c24f9a289e3bce
Arani Dasgupta <rivudasgupta66@gmail.com>
2026-04-28 03:46:10 +0000

    Binary search code

---
+++ Search/binarysearch.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int binarySearch(int arr[],int size,int key){
+    int s = 0;
+    int e = size-1;
+    int mid = (s+e)/2;
+
+    while(s<=e){
+        if(arr[mid] == key){
+            return mid;
+        }
+        //go to right part
+        if(key > arr[mid])
+        {
+            s = mid+1;
+        }
+        else{
+            e = e-1;
+        }
+        mid = (s+e)/2;
+    }
+    return -1;
+}
+
+
+int main(){
+    int even[6] = {2,4,6,8,12,18};
+    int odd[5] = {3,8,11,14,16};
+    int evenIndex = binarySearch(even,6,12);
+    cout<<"Index of 18 is:"<<evenIndex<<endl;
+
+    int oddIndex = binarySearch(odd,5,8);
+    cout<<"Index of 8 is:"<<oddIndex<<endl;
+
+
+
+    return 0;
+}
